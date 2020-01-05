ab673862822988ea829776bf7c17d7a05a4e6e4c
Stephen Taylor <me@meandmycode.com>
2020-01-05 16:05:59 +0000

    removed xmlns from svg icons

--- src/components/icons/check-list.tsx
+++ src/components/icons/check-list.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 export const CheckListIcon = ({ className, ...etc }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    className={className}
-    {...etc}
-  >
+  <svg viewBox="0 0 24 24" className={className} {...etc}>
     <path d="M9 21H0v-2h9v2zm7-3l-4-3-1 1 5 5 8-8-1-2-7 7zm-7-1H0v-2h9v2zm8-4H0v-2h17v2zm7-4H0V7h24v2zm0-4H0V3h24v2z" />
   </svg>
 );
--- src/components/icons/menu.tsx
+++ src/components/icons/menu.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 export const MenuIcon = ({ className, ...etc }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    className={className}
-    {...etc}
-  >
-    <path d="M24 6H0V2h24v4zm0 4H0v4h24v-4zm0 8H0v4h24v-4z"/>
+  <svg viewBox="0 0 24 24" className={className} {...etc}>
+    <path d="M24 6H0V2h24v4zm0 4H0v4h24v-4zm0 8H0v4h24v-4z" />
   </svg>
 );
