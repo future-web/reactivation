bc088a9c0fe19ae126bdeb414d620ec39c70aa94
Stephen Taylor <me@meandmycode.com>
2020-01-05 13:59:38 +0000

    added use-loader hook

---
+++ src/hooks/use-loader.ts
@@ -0,0 +1,27 @@
+import { useState, useCallback } from 'react';
+
+interface ActionType {
+  <T>(value: Promise<T>): Promise<T>;
+}
+
+/**
+ * Provides a loading indicator by observing asynchronous tasks such that the
+ * loading state is true if at least one task is still in a pending state
+ */
+export function useLoader(): [boolean, ActionType] {
+  const [pendingTasks, setPendingTasks] = useState(0);
+
+  const addTask = useCallback(async <T>(task: Promise<T>): Promise<T> => {
+    setPendingTasks(count => count + 1);
+
+    try {
+      return await task;
+    } finally {
+      setPendingTasks(count => count - 1);
+    }
+  }, [setPendingTasks]);
+
+  const isLoading = pendingTasks > 0;
+
+  return [isLoading, addTask];
+}
